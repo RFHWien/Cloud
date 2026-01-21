81e64bf3ded3a515bbea30ab88127b8c61f30943
RFHWien <rainer.adelsberger@edu.fh-wien.ac.at>
2026-01-21 18:07:57 +0100

    R3: Cost control using small EC2 instance types

--- launch_template.tf
+++ launch_template.tf
@@ -1,9 +1,10 @@
 # R1: Webserver implemented using Docker (NGINX)
+# R3: Cost control using small EC2 instance types
 
 resource "aws_launch_template" "web" {
   name_prefix   = "web-"
   image_id      = "ami-1234567890abcdef0"  # Placeholder AMI
-  instance_type = "t3.micro"
+  instance_type = "t3.micro"               # Cost-optimized instance
 
   user_data = base64encode(<<EOF
 #!/bin/bash
